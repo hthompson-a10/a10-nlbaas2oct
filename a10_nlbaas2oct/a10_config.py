6d539278bbba4b74cdf22c64ec781f8eb674c2f9
Hunter Thompson <hthompson@a10networks.com>
2021-02-02 02:34:56 +0000

    Added test framework

--- a10_nlbaas2oct/a10_config.py
+++ a10_nlbaas2oct/a10_config.py
@@ -1,4 +1,4 @@
-# Copyright 2014,  Doug Wiegley,  A10 Networks.
+# Copyright 2020 A10 Networks, Inc.
 #
 #    Licensed under the Apache License, Version 2.0 (the "License"); you may
 #    not use this file except in compliance with the License. You may obtain
